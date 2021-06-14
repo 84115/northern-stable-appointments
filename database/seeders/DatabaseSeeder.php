2634a854b751e8e086c3f9cf94b93bdd516d72e0
James Ball <jamesballsemail@gmail.com>
2021-06-14 13:40:11 +0100

    Main seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ServiceSeeder::class,
-            AppointmentSeeder::class,
-            AppointmentHostSeeder::class,
         ]);
     }
 }
