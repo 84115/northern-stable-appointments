05ead8d2baa1c603a769fd3fb44758ae54ce3e78
James Ball <jamesballsemail@gmail.com>
2021-06-14 13:29:08 +0100

    Inital Factory/Seeders

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Service;
 
 class ServiceSeeder extends Seeder
 {
@@ -13,6 +14,6 @@ class ServiceSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Service::factory(5)->create();
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
-class AppointmentHostSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,6 +14,6 @@ class AppointmentHostSeeder extends Seeder
      */
     public function run()
     {
-        //
+         User::factory(5)->create();
     }
 }
